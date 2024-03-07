a24172d7de0eaf723f3dbbd506c4670f3de6024c
seanf2UH <156386596+seanf2UH@users.noreply.github.com>
2024-03-07 08:56:01 -1000

    simple-schema

--- app/imports/api/PeopleCollection.js
+++ app/imports/api/PeopleCollection.js
@@ -1,3 +1,16 @@
 import { Mongo } from 'meteor/mongo';
+import 'meteor/aldeed:collection2/static';
+import SimpleSchema from 'simpl-schema';
+
+const peopleSchema = new SimpleSchema(
+  {
+    first: String,
+    last: String,
+    age: SimpleSchema.Integer,
+    city: String
+  }
+);
 
 export const PeopleCollection = new Mongo.Collection('People');
+
+PeopleCollection.attachSchema(peopleSchema);
